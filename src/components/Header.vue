c940077591f2a821cbe8d8f1ebebfdf6b301c8f8
Cristhian Sevilla <crissev17@gmail.com>
2024-04-01 20:39:11 -0600

    Detectando la pagina de inicio

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { computed } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+
+const route = useRoute();
+
+const paginaInicio = computed(() => route.name === "inicio");
 </script>
 
 <template>
-  <header class="bg-slate-800">
+  <header class="bg-slate-800" :class="{ header: paginaInicio }">
     <div class="mx-auto container px-5 py-16">
       <div class="flex justify-between items-center">
         <div>
@@ -29,6 +34,7 @@ import { RouterLink } from "vue-router";
         </nav>
       </div>
       <form
+        v-if="paginaInicio"
         class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
       >
         <div class="space-y-4">
@@ -65,3 +71,11 @@ import { RouterLink } from "vue-router";
     </div>
   </header>
 </template>
+
+<style>
+.header {
+  background-image: url("/img/bg.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
