d3868a743fc8403fb063d176d396778ee70b1954
Cristhian Sevilla <crissev17@gmail.com>
2024-04-03 09:49:12 -0600

    Se agregaron computed properties a las condiciones de la consulta de recetas

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -1,4 +1,4 @@
-import { ref, onMounted, reactive } from "vue";
+import { ref, onMounted, reactive, computed } from "vue";
 import { defineStore } from "pinia";
 import APIServices from "../services/APIServices";
 
@@ -18,23 +18,27 @@ export const useBebidasStore = defineStore("bebidas", () => {
     categorias.value = drinks;
   });
 
+  const porNombre = computed(() => {
+    return busqueda.nombre !== "" && busqueda.categoria === "";
+  });
+  const porCategoria = computed(() => {
+    return busqueda.categoria !== "" && busqueda.nombre === "";
+  });
+  const porNombreYcategoria = computed(() => {
+    return busqueda.nombre !== "" && busqueda.categoria !== "";
+  });
+
   async function obtenerRecetas() {
     try {
-      if (busqueda.nombre !== "" && busqueda.categoria === "") {
-        const {
-          data: { drinks },
-        } = await APIServices.recetasNombre(busqueda.nombre);
-        recetas.value = drinks;
-      } else if (busqueda.categoria !== "" && busqueda.nombre === "") {
-        const {
-          data: { drinks },
-        } = await APIServices.recetasCategoria(busqueda.categoria);
-        recetas.value = drinks;
-      } else if (busqueda.nombre !== "" && busqueda.categoria !== "") {
-        const {
-          data: { drinks },
-        } = await APIServices.recetasCatYnom(busqueda);
-        recetas.value = drinks;
+      if (porNombre) {
+        const { data } = await APIServices.recetasNombre(busqueda.nombre);
+        recetas.value = data.drinks;
+      } else if (porCategoria) {
+        const { data } = await APIServices.recetasCategoria(busqueda.categoria);
+        recetas.value = data.drinks;
+      } else if (porNombreYcategoria) {
+        const { data } = await APIServices.recetasCatYnom(busqueda);
+        recetas.value = data.drinks;
       }
     } catch (error) {
       console.log(error);
