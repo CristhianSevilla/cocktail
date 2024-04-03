0cf1b8b25111376cafbf8c4133d222c23c9ae97a
Cristhian Sevilla <crissev17@gmail.com>
2024-04-02 20:19:43 -0600

    Placeholder de nombre o ingrediente

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -56,7 +56,7 @@ const handleSubmit = () => {
             class="p-3 w-full rounded-lg focus:outline-none"
             id="ingrediente"
             type="text"
-            placeholder="Nombre o Ingrediente: ej. Vodka, Tequila, etc."
+            placeholder="Ej. Vodka, Tequila, etc."
             v-model="store.busqueda.nombre"
           />
         </div>
