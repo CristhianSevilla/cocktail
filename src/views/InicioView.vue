54433b3a3c5745a1ba037bc49919a03131666ab6
Cristhian Sevilla <crissev17@gmail.com>
2024-04-04 09:51:02 -0600

    Primeros pasos mostrando recetas

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup>
+import Receta from "../components/Receta.vue";
+import { useBebidasStore } from "../stores/bebidas";
+
+const bebidas = useBebidasStore();
+</script>
 
 <template>
-  <h1 class="text-6xl font-extrabold">Recetas</h1>
+  <div class="px-3 xl:px-16 mb-5">
+    <h1 class="text-4xl font-extrabold">Recetas</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 mt-10 gap-10">
+      <Receta
+        v-for="receta in bebidas.recetas"
+        :key="receta.idDrink"
+        :receta="receta"
+      />
+    </div>
+  </div>
 </template>
