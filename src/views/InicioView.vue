e726194e2a8e500da6822c69b49b271fa0a62874
Cristhian Sevilla <crissev17@gmail.com>
2024-04-04 10:47:08 -0600

    Stash apply

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,17 +1,18 @@
 <script setup>
+import { useBebidasStore } from "@/stores/bebidas";
 import Receta from "../components/Receta.vue";
-import { useBebidasStore } from "../stores/bebidas";
 
 const bebidas = useBebidasStore();
 </script>
 
 <template>
-  <div class="px-3 xl:px-16 mb-5">
-    <h1 class="text-4xl font-extrabold">Recetas</h1>
-    <div class="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 mt-10 gap-10">
+  <div class="mx-3 md:mx-5 xl:mx-10">
+    <h1 class="text-5xl font-extrabold">Recetas</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-10">
       <Receta
         v-for="receta in bebidas.recetas"
-        :key="receta.idDrink"
+        :key="receta.id"
         :receta="receta"
       />
     </div>
