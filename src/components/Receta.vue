c473653c26f226cc0302cae482190defddf1a8a4
Cristhian Sevilla <crissev17@gmail.com>
2024-04-04 11:14:35 -0600

    Creando las cards de recetas

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -7,7 +7,21 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <h2>{{ receta.strDrink }}</h2>
+  <div class="border shadow-lg overflow-hidden rounded bg-orange-100">
+    <div>
+      <img
+        class="hover:scale-105 transition-transform hover:rotate-1"
+        :src="receta.strDrinkThumb"
+        alt="Imagen Receta"
+      />
+    </div>
+    <div class="p-5">
+      <h2 class="text-2xl truncate font-extrabold">{{ receta.strDrink }}</h2>
+      <button
+        class="bg-orange-500 hover:bg-orange-600 transition-all mt-5 w-full p-3 font-bold text-white text-lg rounded"
+      >
+        Ver Receta
+      </button>
+    </div>
   </div>
 </template>
--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -9,7 +9,7 @@ const bebidas = useBebidasStore();
   <div class="mx-3 md:mx-5 xl:mx-10">
     <h1 class="text-5xl font-extrabold">Recetas</h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-10 gap-10">
       <Receta
         v-for="receta in bebidas.recetas"
         :key="receta.id"
