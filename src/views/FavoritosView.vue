726c258bcc9731a1fc0a7ab2d617b9f1eeb460c0
Cristhian Sevilla <crissev17@gmail.com>
2024-04-24 18:00:28 -0600

    Mostrando los favoritos

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import Receta from "@/components/Receta.vue";
+import { useFavoritosStore } from "@/stores/favoritos";
+
+const favoritos = useFavoritosStore();
+</script>
 
 <template>
   <h1 class="text-6xl font-extrabold">Mis Favoritos</h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-10 gap-10">
+    <Receta
+      v-for="receta in favoritos.favoritos"
+      :key="receta.id"
+      :receta="receta"
+    />
+  </div>
 </template>
