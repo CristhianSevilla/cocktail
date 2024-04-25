7d03318c2f6f16f8268b4161a1fed75138dd90f2
Cristhian Sevilla <crissev17@gmail.com>
2024-04-24 18:43:45 -0600

    Mostrando un texto si aun no existen favoritos

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -7,7 +7,16 @@ const favoritos = useFavoritosStore();
 
 <template>
   <h1 class="text-6xl font-extrabold">Mis Favoritos</h1>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-10 gap-10">
+  <h2
+    v-if="favoritos.noFavoritos"
+    class="mt-10 text-2xl font-semibold text-center"
+  >
+    <span class="text-orange-600">¡Ups!</span> Aún no agregas Favoritos
+  </h2>
+  <div
+    v-else
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-10 gap-10"
+  >
     <Receta
       v-for="receta in favoritos.favoritos"
       :key="receta.id"
